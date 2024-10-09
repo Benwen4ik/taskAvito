cf988016abf172bb3ace791c0763055192a572d1
Artem Garanin <artemgar.garanin@gmail.com>
2024-10-09 18:40:35 +0300

    add swagger

--- src/main/java/com/tech/linkShort/LinkShortApplication.java
+++ src/main/java/com/tech/linkShort/LinkShortApplication.java
@@ -1,12 +1,25 @@
 package com.tech.linkShort;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+//import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.sqids.Sqids;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LinkShortApplication {
+
+	@Value("${server.port}")
+	private String PORT_SERVER;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkShortApplication.class, args);
@@ -16,5 +29,29 @@ public class LinkShortApplication {
 	public Sqids createSqids(){
 		return Sqids.builder().alphabet("k3G7QAe51FCsPW92uEOyq4Bg6Sp8YzVTmnU0liwDdHXLajZrfxNhobJIRcMvKt").build();
 	}
+
+	/*@Bean
+	public GroupedOpenApi publicUserApi() {
+		return GroupedOpenApi.builder()
+				.group("Users")
+				.pathsToMatch("/users/**")
+				.build();
+	}
+
+	@Bean
+	public OpenAPI customOpenApi(@Value("${application.description}")String appDescription,
+								 @Value("${application.version}")String appVersion) {
+		return new OpenAPI().info(new Info().title("Link Short")
+						.version(appVersion)
+						.description(appDescription)
+						.license(new License().name("Apache 2.0")
+								.url("http://springdoc.org"))
+						.contact(new Contact().name("Artem")
+								.email("test@gmail.com")))
+				.servers(List.of(new Server().url("http://localhost:" + PORT_SERVER)
+								.description("Dev service"),
+						new Server().url("http://localhost:8082")
+								.description("Beta service")));
+	}*/
 
 }
--- src/main/java/com/tech/linkShort/controllers/LinkController.java
+++ src/main/java/com/tech/linkShort/controllers/LinkController.java
@@ -6,6 +6,13 @@ import com.tech.linkShort.services.LinkService;
 import com.tech.linkShort.util.LinkException;
 import com.tech.linkShort.util.LinkValidation;
 import com.tech.linkShort.util.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +26,7 @@ import static com.tech.linkShort.util.ErrorUtil.returnErrorsToClient;
 
 @RestController
 @RequestMapping("/api/link")
+@Tag(name = "Link", description = "link api")
 public class LinkController {
 
     private final LinkService linkService;
@@ -32,6 +40,17 @@ public class LinkController {
     }
 
     @PostMapping("/add")
+    @Operation(tags = "link", summary = "get token of link")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Get token of link",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = LinkDTO.class)))
+                    })
+    })
     public ResponseEntity<Response> createLink(@RequestBody LinkDTO linkDTO, BindingResult bindingResult) {
         linkValidation.validate(Link.builder().fullURL(linkDTO.getFullURL()).build(),bindingResult);
         if (bindingResult.hasErrors()) {
@@ -43,10 +62,21 @@ public class LinkController {
     }
 
     @GetMapping("/{token}")
+    @Operation(tags = "link", summary = "get url by token")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "Found",
+                    description = "Get URL by token",
+                    content = {
+                            @Content(
+                                    mediaType = "html",
+                                    array = @ArraySchema(schema = @Schema(implementation = LinkDTO.class)))
+                    })
+    })
     public ResponseEntity<Void> getLinkByToket(@PathVariable String token){
         Link link = linkService.searchByToken(token);
         if(link == null){
-            //return ;
+            //return ;  // TODO
         }
         //  return LinkDTO.builder().fullURL(link.getFullURL()).build();
         return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(link.getFullURL())).build();
